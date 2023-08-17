809a0aef41ba8535016f9ac27afc255505a3a6ea
Yurii-ios <58275271+Yurii-ios@users.noreply.github.com>
2023-08-17 13:08:32 +0200

    feature: add title label

--- copyscreen/ViewController.swift
+++ copyscreen/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .red
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        style()
+        layout()
+    }
+    
+    func style() {
         view.backgroundColor = .orange
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func layout() {
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
 }
