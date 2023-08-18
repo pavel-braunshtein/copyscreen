f72d2468cd93edf64a67170a3096109912d6b526
pavelbraun333 <pavelbraun333@gmail.com>
2023-08-18 10:47:59 +0300

    feature: switched background colour

--- copyscreen/ViewController.swift
+++ copyscreen/ViewController.swift
@@ -12,6 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
     }
 }
