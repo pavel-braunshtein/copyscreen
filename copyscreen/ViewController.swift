28e5ae897cbf30d41a79ffaef7d58ef010243f09
pavelbraun333 <pavelbraun333@gmail.com>
2023-08-23 20:16:31 +0300

    feature: did something new

--- copyscreen/ViewController.swift
+++ copyscreen/ViewController.swift
@@ -10,6 +10,7 @@ import SwiftUI
 final class MovieDetailsViewController: UIViewController {
 
    let stackView = UIStackView()
+   let stackView2 = UIStackView()
     
     
     let dividerView = UIView()
@@ -27,23 +28,26 @@ final class MovieDetailsViewController: UIViewController {
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "Title"
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.text = "EVON"
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
         label.textAlignment = .center
-        label.textColor = .red
+        label.textColor = .black
         
         return label
     }()
     
+    
     let subtitleLabel: UILabel = {
         let label = UILabel()
-        label.text = "subtitle"
-        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.numberOfLines = 2
+        label.text = "Discover upcoming events\nnear you"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         label.textAlignment = .center
-        label.textColor = .red
+        label.textColor = .systemGray
         
         return label
     }()
+    
     
     let usernameTextField: UITextField = {
         let usernameTextField = UITextField()
@@ -53,6 +57,8 @@ final class MovieDetailsViewController: UIViewController {
         
         return usernameTextField
     }()
+    
+    
     
     let passwordTextField: UITextField = {
         let passwordTextField = UITextField()
@@ -64,17 +70,54 @@ final class MovieDetailsViewController: UIViewController {
         return passwordTextField
     }()
     
+    
     let button: UIButton = {
         let button = UIButton()
-        button.setTitle("Click", for: .normal)
-        button.backgroundColor = .link
-        button.layer.cornerRadius = 5
-        button.setTitleColor(.red, for: .highlighted)
         
-        
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 2
+        button.setTitleColor(.blue, for: .highlighted)
         
         return button
     }()
+    
+    
+    let buttonn: UIButton = {
+        let buttonn = UIButton()
+        
+        buttonn.setTitle("Log In", for: .normal)
+        buttonn.backgroundColor = .white
+        buttonn.setTitleColor(.link, for: .normal)
+        buttonn.layer.borderColor = UIColor.link.cgColor
+        buttonn.layer.borderWidth = CGFloat(1)
+        buttonn.layer.cornerRadius = 2
+        buttonn.setTitleColor(.blue, for: .highlighted)
+        
+        return buttonn
+    }()
+    
+    
+    let button3: UIButton = {
+        let button3 = UIButton()
+        
+        button3.setTitle("Skip for now", for: .normal)
+        button3.backgroundColor = .white
+        button3.setTitleColor(.link, for: .normal)
+        button3.setTitleColor(.blue, for: .highlighted)
+        button3.layer.cornerRadius = 2
+
+        return button3
+    }()
+    
+    
+    let logo: UIImageView = {
+        let logo = UIImageView(image: UIImage(named: "Evon2"))
+        logo.layer.cornerRadius = 365
+        return logo
+    }()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -82,19 +125,39 @@ final class MovieDetailsViewController: UIViewController {
         layout()
     }
     
+    
+    
+    
     func style() {
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
         
         stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.distribution = .fillEqually
-        stackView.layer.borderColor = UIColor.red.cgColor
-        stackView.layer.borderWidth = CGFloat(1)
+        stackView.spacing = 0
+        stackView.distribution = .fillProportionally
+//        stackView.layer.borderColor = UIColor.red.cgColor
+//        stackView.layer.borderWidth = CGFloat(1)
+        
+        
+        
+        stackView2.axis = .vertical
+        stackView2.spacing = 10
+        stackView2.distribution = .fillProportionally
+//        stackView2.layer.borderColor = UIColor.blue.cgColor
+//        stackView2.layer.borderWidth = CGFloat(1)
+        
+        
+        
+        
         
         passwordToggleButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
         passwordToggleButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .selected)
         passwordTextField.rightView = passwordToggleButton
         passwordTextField.rightViewMode = .always
+        
+        
+        
+        
+        
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -103,6 +166,11 @@ final class MovieDetailsViewController: UIViewController {
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         extraTitle.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        buttonn.translatesAutoresizingMaskIntoConstraints = false
+        button3.translatesAutoresizingMaskIntoConstraints = false
+        stackView2.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func layout() {
@@ -114,20 +182,54 @@ final class MovieDetailsViewController: UIViewController {
 //        view.addSubview(passwordTextField)
 //        view.addSubview(extraTitle)
         view.addSubview(stackView)
+        stackView.addArrangedSubview(logo)
         stackView.addArrangedSubview(titleLabel)
         stackView.addArrangedSubview(subtitleLabel)
-        stackView.addArrangedSubview(usernameTextField)
+        //stackView.addArrangedSubview(usernameTextField)
         stackView.addArrangedSubview(dividerView)
-        stackView.addArrangedSubview(passwordTextField)
-        stackView.addArrangedSubview(extraTitle)
-        stackView.addArrangedSubview(button)
+        //stackView.addArrangedSubview(passwordTextField)
+        //stackView.addArrangedSubview(extraTitle)
+        view.addSubview(stackView2)
+        stackView2.addArrangedSubview(button)
+        stackView2.addArrangedSubview(buttonn)
+        stackView2.addArrangedSubview(button3)
         
+//
+//        NSLayoutConstraint.activate([
+//            logo.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+//            logo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+//            logo.trailingAnchor.constraint(equalTo:view.trailingAnchor, constant: -16),
+//            logo.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -550),
+//            button3.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+//
+//        ])
+        
+       
+        NSLayoutConstraint.activate([
+            logo.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 30),
+            logo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logo.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -90),
+            
+            titleLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 80)
+        ])
         
         NSLayoutConstraint.activate([
-           // stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 10),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 140),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 80),
+//            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 10),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+           
+            
+        ])
+        NSLayoutConstraint.activate([
+            stackView2.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 30),
+            stackView2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            stackView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            stackView2.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 200)
+           
             
         ])
         
